9a9e110ac5979bad5e5a58860a102ca366a2cad3
rnyorke <rnyorke@gmail.com>
2014-10-16 16:03:52 -0500

    Started my house

--- 70-100pt.py
+++ 70-100pt.py
@@ -14,3 +14,27 @@
 
 # Minus 5pts if your code has no comments
 # Minus 10pts if you only commit once to github
+from Tkinter import *
+root = Tk()
+drawpad = Canvas(root, width=800,height=600, background='white')
+drawpad.grid(row=0, column=1)
+# create_oval(x,y,width,height,fill color)
+oval = drawpad.create_oval(10, 50, 100, 100, fill='green')
+
+#create_square(top left x,top left y, bottom right x, bottom right y, fill color)
+#this is the base of the house
+
+square = drawpad.create_rectangle(300,300,600,700, fill='red')
+#this square is the upper left window currently
+sq2 = drawpad.create_rectangle(325,325,375,350, fill='black')
+
+
+#create_line(top left x,top left y, bottom right x, bottom right y, fill color)
+
+#this is the line that connects the left part of the house to the top of the roof
+line = drawpad.create_line(300, 300, 450, 100)
+#this is the line that connects the right part of the house to the top of the roof
+line2 = drawpad.create_line(600, 300, 450, 100)
+
+root.mainloop()
+
